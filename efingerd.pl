f630339ed959116b680d2ed2f6aebf3d8c9935d7
Reuben Thomas <rrt@sc3d.org>
2013-05-30 13:27:29 +0100

    efingerd.pl: simplify, and exit(1) if interrupted

--- efingerd.pl
+++ efingerd.pl
@@ -13,32 +13,25 @@ my $EFINGER_LUSER = "/etc/$PROGRAM/luser";
 my $EFINGER_NOUSER = "/etc/$PROGRAM/nouser";
 my $EFINGER_USER_FILE = ".finger";
 
-
-sub finish {
-  close(STDIN);
-  close(STDOUT);
-  closelog();
-  exit(0);
-}
-
-sub finger {
-  my $user = shift;
-  my $prog = $EFINGER_NOUSER;
-  if (length($user) == 0) {
-    $prog = $EFINGER_LIST;
-  } else {
-    my ($dir) = (getpwnam($user))[7];
-    if ($dir && stat("$dir/$EFINGER_USER_FILE")) {
-      $prog = $EFINGER_LUSER;
-    }
-  }
-  system $prog, $user;
-}
-
 die("$PROGRAM version $VERSION\nNot for interactive use.\n") if -t STDIN;
 
 openlog($PROGRAM, "pid", LOG_DAEMON);
-$SIG{XCPU} = &finish;
+$SIG{XCPU} = sub {
+  close(STDIN);
+  close(STDOUT);
+  closelog();
+  exit(1);
+};
+
 my $user = <STDIN>;
 $user =~ s/\r?\n//; # Some buggy clients omit the \r
-finger($user);
+my $prog = $EFINGER_NOUSER;
+if (length($user) == 0) {
+  $prog = $EFINGER_LIST;
+} else {
+  my ($dir) = (getpwnam($user))[7];
+  if ($dir && stat("$dir/$EFINGER_USER_FILE")) {
+    $prog = $EFINGER_LUSER;
+  }
+}
+system $prog, $user;
