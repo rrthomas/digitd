4ae445f2ebc3eec3b7714de370ab46e6f2607c43
Reuben Thomas <rrt@sc3d.org>
2013-05-30 01:33:52 +0100

    efingerd.c: reformat code for consistency

--- efingerd.c
+++ efingerd.c
@@ -50,7 +50,7 @@
  * ==================================================================
  */
 /* SERVICE BEHAVIOR     */
-unsigned char   resolve_addr;		/* reverse lookup addresses	*/
+unsigned char resolve_addr;		/* reverse lookup addresses	*/
 const unsigned short client_timeout = 60; /* number of seconds till disconnect */
 
 
@@ -59,10 +59,10 @@ const unsigned short client_timeout = 60; /* number of seconds till disconnect *
  *	violently kills a socket
  * ------------------------------------------------------------------
  */
-static void killsock (int s)
+static void killsock(int s)
 {
-	shutdown (s, 2);
-	close (s);
+    shutdown(s, 2);
+    close(s);
 }
 
 
@@ -70,26 +70,26 @@ static void killsock (int s)
  * get_request	:	a fgets for file descriptors
  * ------------------------------------------------------------------
  */
-static int get_request (int d, char buffer[], u_short len)
+static int get_request(int d, char buffer[], u_short len)
 {
-	u_short i;
-	char ch;
+    u_short i;
+    char ch;
 
-	memset (buffer, 0, len);
-	for (i = 0; i < len; i++) {
-		if (read (d, &ch, 1) != 1)
-			return -1;
-		else if (ch == '\n') /* some buggy clients send only \n */
-			break;
-		else if (ch == '\r') {
-			read (d, &ch, 1); /* should read the following \n */
-			break;
-		}
-		else
-			buffer[i] = ch;
+    memset(buffer, 0, len);
+    for (i = 0; i < len; i++) {
+	if (read(d, &ch, 1) != 1)
+	    return -1;
+	else if (ch == '\n') /* some buggy clients send only \n */
+	    break;
+	else if (ch == '\r') {
+	    read(d, &ch, 1); /* should read the following \n */
+	    break;
 	}
-	buffer[i] = '\0';
-	return i;
+	else
+	    buffer[i] = ch;
+    }
+    buffer[i] = '\0';
+    return i;
 }
 
 
@@ -98,9 +98,9 @@ static int get_request (int d, char buffer[], u_short len)
  *	send a reply back to the client..
  * ------------------------------------------------------------------
  */
-static void client_reply (int sd, char *outcome)
+static void client_reply(int sd, char *outcome)
 {
-	write (sd, outcome, strlen (outcome));
+    write(sd, outcome, strlen(outcome));
 }
 
 
@@ -108,16 +108,17 @@ static void client_reply (int sd, char *outcome)
  * usage:
  * ------------------------------------------------------------------
  */
-static void	usage (char *progname)
+static void usage(char *progname)
 {
-	 fprintf (stderr, "usage: %s [options]\n"
-			"   --help     This information.\n"
-			"   --version  Print version information and exit.\n"
-			"   -t X       Time to keep connection.\n"
-			"              ex: -t 25  maintain connections for up to 25 seconds.\n"
-			"   -n         Do not lookup addresses, use IP numbers instead.\n",
-			progname);
-	exit (0);
+    fprintf(stderr,
+	    "usage: %s [options]\n"
+	    "   --help     This information.\n"
+	    "   --version  Print version information and exit.\n"
+	    "   -t X       Time to keep connection.\n"
+	    "              ex: -t 25  maintain connections for up to 25 seconds.\n"
+	    "   -n         Do not lookup addresses, use IP numbers instead.\n",
+	    progname);
+    exit(0);
 }
 
 
@@ -126,10 +127,10 @@ static void	usage (char *progname)
  *	wouldn't want to disappoint anyone.
  * ------------------------------------------------------------------
  */
-static void print_version (void)
+static void print_version(void)
 {
-	fprintf (stderr, "efingerd %s\n", ID_VERSION );
-	exit (0);
+    fprintf(stderr, "efingerd %s\n", ID_VERSION);
+    exit(0);
 }
 
 
@@ -245,46 +246,45 @@ static void inetd_service(int sd_in, int sd_out)
 }
 
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	u_short i;
+    u_short i;
 
-	resolve_addr = 1;
+    resolve_addr = 1;
 
-	for (i = 1; i < argc; i++) {
-		if (argv[i][0] == '-') {
-			switch (argv[i][1]) {
-				case 'v':
-					print_version ();
+    for (i = 1; i < argc; i++) {
+	if (argv[i][0] == '-') {
+	    switch (argv[i][1]) {
+	    case 'v':
+		print_version();
+		break;
 
-				case 'n':
-					resolve_addr = 0;
-					break;
+	    case 'n':
+		resolve_addr = 0;
+		break;
 
-				case 'h':
-					usage (argv[0]);
+	    case 'h':
+		usage(argv[0]);
+		break;
 
-				case '-':
-
-					if (strncmp ("version", ( argv[i]+2), 7) == 0)
-						print_version ();
-
-					else if (strcmp ("help", ( argv[i]+2)) == 0)
-						usage (argv[0]);
-
-					break;
-			}
-		}
+	    case '-':
+		if (strncmp("version", argv[i]+2, 7) == 0)
+		    print_version();
+		else if (strcmp("help", argv[i]+2) == 0)
+		    usage(argv[0]);
+		break;
+	    }
 	}
+    }
 
-	openlog ("efingerd", LOG_PID, LOG_DAEMON);
-	alarm (client_timeout);
-	signal (SIGALRM, killtic);
+    openlog("efingerd", LOG_PID, LOG_DAEMON);
+    alarm(client_timeout);
+    signal(SIGALRM, killtic);
 
-	inetd_service (0, 1);
+    inetd_service(0, 1);
 
-	killsock (0);
-	killsock (1);
-	closelog ();
-	exit (-1);
+    killsock(0);
+    killsock(1);
+    closelog();
+    exit(-1);
 }
